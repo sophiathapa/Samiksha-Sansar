d975daa4e27e5408905e28e0bdba23f6b50bc800
sophiathapa <sophia.thapa55@gmail.com>
2026-08-14 15:44:47 +0545

    fix: update cookie settings for production

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -60,8 +60,8 @@ const login = async (req, res) => {
   const token = jwt.sign({ id: user?._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
