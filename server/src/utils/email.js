3b4667b91679b5d9ab5b02d5f962bcd2d58c304e
Sophia Thapa Magar <109805189+sophiathapa@users.noreply.github.com>
2026-08-15 10:46:54 +0545

    Change email transport to secure connection

--- server/src/utils/email.js
+++ server/src/utils/email.js
@@ -5,7 +5,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: false, // true for 465
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
